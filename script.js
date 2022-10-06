13fb6e4536937c8917bab4d9d9289fb45ab79ac2
Michael <mvale005@gmail.com>
2022-10-05 22:46:41 -0500

    added buttons, started scoring and rounds

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //global variables
 let choiceArray= ["paper","rock","scissors"];
-let count = 0;
+let pcScore = 0;
+let personScore = 0;
+
 // I'm putting the selector and createElement up here so it doesn't only shows your current choice and not a full list
 let scoreBoxLeft = document.querySelector(".score-box-left");
 let personChoiceDisplay = document.createElement("p");
+
 //same for the computer choice
 let scoreBoxRight = document.querySelector(".score-box-right");
 let compChoiceDisplay = document.createElement("p");
@@ -12,116 +15,121 @@ let compChoiceDisplay = document.createElement("p");
 //three button click events that will fire off personChoice method which returns
 // the button the user selected
 let rockBtn = document.getElementById("btn-rock");
- rockBtn.addEventListener('click',checkPersonChoice);
-
+ rockBtn.addEventListener('click',playRound);
+ 
 let paperBtn = document.getElementById("btn-paper");
-paperBtn.addEventListener('click',checkPersonChoice);
+paperBtn.addEventListener('click',playRound);
 
 let scissorsBtn = document.getElementById("btn-scissors");
-scissorsBtn.addEventListener('click',checkPersonChoice);
+scissorsBtn.addEventListener('click',playRound);
 
-//checking person choice function and displaying it ***********************
-function checkPersonChoice(e){
+function playRound(e){
+
+  if(pcScore==5 || personScore==5){
+    if(pcScore>personScore){
+      console.log("computer wins the title");
+    }else if(personScore>pcScore){
+      console.log("player wins the title");}
+      else {console.log("wow, it was tie")}
+
+    }
+
   
-  let personChoice=  e.target.innerText.toLowerCase();
-  console.log(personChoice);
+  let userValue = e.target.innerText.toLowerCase();
+  upDateBoxLeft(userValue);
+  let pcValue = getComputerChoice();      
 
+  //check to see who won or if it's a tie
 
-for(let i= 0;i<choiceArray.length;i++) {
-  if (personChoice == choiceArray[i]){
-    console.log("your pick was " + choiceArray[i]);
-  } 
+  let isTie = checkForTie(userValue,pcValue);
+  if(isTie==true){
+    pcScore++;personScore++;
+    console.log("You both picked " +  userValue+ ", that's a tie!!");
+    console.log("PC "+ pcScore + " person " + personScore)
+    
+  return;}
+
+    let pcWon = didPCWin(userValue,pcValue);
+    
+    if (pcWon==true){pcScore++;
+      console.log("Computer wins! pc "+ pcScore + " person " + personScore);console.log("");
+      
+    }else {personScore++;
+      console.log("Player wins! pc " + pcScore + " person " + personScore);console.log("");
+      }
 }
 
 
-personChoiceDisplay.textContent = personChoice;
-scoreBoxLeft.append(personChoiceDisplay);
-
-let getPCChoice = setTimeout(getComputerChoice,10);
-
-return personChoice;
+function upDateBoxLeft(personChoice){
+  console.log("Player chose " + personChoice);
+  personChoiceDisplay.textContent = "";
+  personChoiceDisplay.textContent += personChoice +"--" + " " + "score: "+ personScore;
+  scoreBoxLeft.append(personChoiceDisplay);
 
 }
 
 function getComputerChoice(){
   let randNum = getRandomInt(3);
   let compChoice = choiceArray[randNum];
-  console.log("The computer chose " + compChoice);
+  console.log("Computer chose " + compChoice);
+  upDateBoxRight(compChoice);
+  return compChoice;
+}
 
+function upDateBoxRight(compChoice){
 
-
-  compChoiceDisplay.textContent = compChoice
+  compChoiceDisplay.textContent = "";
+  compChoiceDisplay.textContent += compChoice +"--" + " " + "score: "+ pcScore;
   scoreBoxRight.appendChild(compChoiceDisplay);
 
-
-  return compChoice;
-};
+}
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
-};
+}
 
-// function playRound(pcChoice, personChoice){
- 
-// let pcScore = 0;
-// let personScore = 0;
-// let i = 0;
+function checkForTie(pcChoice,personChoice){
+  
+  if(pcChoice == personChoice){
+    
+    console.log("");
+    return true;
+  
+  }
+}
 
-// for(i=0; i<5; i++){
-//     //tie logic
-//   if(pcChoice == personChoice){
-//     console.log("You chose " +  personChoice + ", that's a tie!!");
-//     pcScore++; personScore++;
-//   }
 
-// //pc win logic
-//    if (pcChoice == "paper" && personChoice == "rock") { 
-//    console.log("You chose " + personChoice + ", pc wins!"); 
-//    pcScore++;
-   
-//    }else if(pcChoice == "rock" && personChoice == "scissors"){
-//    console.log("You chose " + personChoice + ", pc wins!");
-//    pcScore++;
-   
-//    }else if(pcChoice == "scissors" && personChoice == "paper")
-//     { console.log("You chose " + personChoice + ", pc wins!");
-// 	pcScore++;
-//   }
-// //player win logic
-// 	 if (personChoice  == "paper" && pcChoice== "rock") { 
-// 	  console.log("You chose " + personChoice + ", player wins!"); 
-// 	  personScore++;
-// 	   } else if (personChoice  == "rock" && pcChoice == "scissors"){ 
-// 	   console.log("You chose " + personChoice + ", player wins!"); 
-// 	   personScore++;
-// 	   } else if(personChoice  == "scissors" && pcChoice == "paper"){ 
-// 	   console.log("You chose " + personChoice + ", player wins!"); 
-// 	   personScore++;
-// 	  }
+
+function didPCWin(personChoice, pcChoice)
+{
+   let isTie = checkForTie(personChoice,pcChoice);
+
+  if(isTie==true) return;
+  if (pcChoice == "paper" && personChoice == "rock") return true;
+  else if(pcChoice == "rock" && personChoice == "scissors") return true;
+  else if (pcChoice == "scissors" && personChoice == "paper") return true;
+  else return false;
+  
+  
+  
+}
+
+
  
  
 //   count = count+1;
 //   console.log("After round " + count + " - PC score = "+ pcScore + ", Player score = "+ personScore );
 //   console.log("");
 
-//   if(i<4){
-//   personChoice = checkPersonChoice();
-//   pcChoice= getComputerChoice();
-//   }
+  // if(i<4){
+  // personChoice = checkPersonChoice();
+  // pcChoice= getComputerChoice();
+  // }
 
-// }
-// console.log("Final Score after round 5: PC score = "+ pcScore + ", Player score = "+ personScore);
+
+//console.log("Final Score after round 5: PC score = "+ pcScore + ", Player score = "+ personScore);
  
-// };
-
-// //this initilizes the game
-// function playGame(){
-// let value = checkPersonChoice();
-// let num = getComputerChoice();
-// playRound(num, value);
 // }
-
-// //calling playing game to start everything
-// playGame();
+// }
 
 
