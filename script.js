7882f64d2b7bbead095a44353c82bc95dc4c8bab
Michael <mvale005@gmail.com>
2022-10-06 16:35:08 -0500

    almost comleted game, need to fix bug when game finishes on a tie

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let choiceArray = ["paper", "rock", "scissors"];
 let pcScore = 0;
 let personScore = 0;
 
+
 // I'm putting the selector and createElement up here so it doesn't only shows your current choice and not a full list
+
 let scoreBoxLeft = document.querySelector(".score-box-left");
 let personChoiceDisplay = document.createElement("p");
 
@@ -11,9 +13,15 @@ let personChoiceDisplay = document.createElement("p");
 let scoreBoxRight = document.querySelector(".score-box-right");
 let compChoiceDisplay = document.createElement("p");
 
+//middle scoreboard to display how the games going
+
+let scoreBoxMiddle = document.querySelector(".score-box-middle");
+let middleBoxDisplay = document.createElement("p");
+
 
 //three button click events that will fire off personChoice method which returns
 // the button the user selected
+
 let rockBtn = document.getElementById("btn-rock");
 rockBtn.addEventListener('click', playRound);
 
@@ -24,66 +32,94 @@ let scissorsBtn = document.getElementById("btn-scissors");
 scissorsBtn.addEventListener('click', playRound);
 
 function playRound(e) {
-
-  if (pcScore == 5 || personScore == 5) {
-    if (pcScore > personScore) {
-      console.log("computer wins the title");
-    } else if (personScore > pcScore) {
-      console.log("player wins the title");
-    }
-    else { console.log("wow, it was tie") }
-
-  }
-
-
+  middleBoxDisplay.style.textAlign = "center";
+  middleBoxDisplay.style.color = "black";
   let userValue = e.target.innerText.toLowerCase();
-  upDateBoxLeft(userValue);
   let pcValue = getComputerChoice();
 
   //check to see who won or if it's a tie
 
   let isTie = checkForTie(userValue, pcValue);
-  if (isTie == true) {
+  if (userValue == pcValue) {
+    if(pcScore ==5 || personScore ==5){
+    overallWinner(userValue,pcValue);
+    return;
+    }else {
     pcScore++; personScore++;
-    console.log("You both picked " + userValue + ", that's a tie!!");
-    console.log("PC " + pcScore + " person " + personScore)
-
+    middleBoxDisplay.textContent = " -- Tie -- ";
+    
+    scoreBoxMiddle.append(middleBoxDisplay);
+    upDateBoxLeft(userValue.toUpperCase());
+    upDateBoxRight(pcValue.toUpperCase());
     return;
   }
+}
 
   let pcWon = didPCWin(userValue, pcValue);
 
   if (pcWon == true) {
     pcScore++;
-    console.log("Computer wins! pc " + pcScore + " person " + personScore); console.log("");
+    middleBoxDisplay.textContent = " -- Computer wins -- ";
+     scoreBoxMiddle.append(middleBoxDisplay);
 
   } else {
     personScore++;
-    console.log("Player wins! pc " + pcScore + " person " + personScore); console.log("");
+    middleBoxDisplay.textContent = " -- Player wins  -- ";
+    scoreBoxMiddle.append(middleBoxDisplay);
+  }
+  if (overallWinner(userValue, pcValue) == true) {
+    pcScore = 0; personScore = 0;
+
+  } else {
+    upDateBoxLeft(userValue.toUpperCase());
+    upDateBoxRight(pcValue.toUpperCase());
   }
 }
 
+function overallWinner(userValue, pcValue) {
+  if (pcScore >= 5 || personScore >= 5) {
+    if (pcScore > personScore) {
+        upDateBoxRight(pcValue);
+        middleBoxDisplay.innerHTML = "Computer was first to 5" + "<br> <br>"  + "Computer wins, Game Over";
+        middleBoxDisplay.style.color = "red";
+        scoreBoxMiddle.append(middleBoxDisplay);
+
+      return true;
+ 
+    } else if (personScore > pcScore) {
+      upDateBoxLeft(userValue);
+      middleBoxDisplay.style.color = "green";
+      middleBoxDisplay.innerHTML = "Player was first to 5" + "<br> <br>" + "Player wins, Game Over";
+      scoreBoxMiddle.append(middleBoxDisplay);
+      return true;
+
+    }
+    else {
+      upDateBoxRight(pcValue);
+      upDateBoxLeft(userValue);
+      return true;
+    }
+  }
+  return false;
+
+}
 
 function upDateBoxLeft(personChoice) {
-  console.log("Player chose " + personChoice);
   personChoiceDisplay.textContent = "";
-  personChoiceDisplay.textContent += personChoice + "--" + " " + "score: " + personScore;
+  personChoiceDisplay.textContent += personChoice + " --" + " " + "Score: " + personScore;
   scoreBoxLeft.append(personChoiceDisplay);
-
 }
 
 function getComputerChoice() {
   let randNum = getRandomInt(3);
   let compChoice = choiceArray[randNum];
-  console.log("Computer chose " + compChoice);
-  upDateBoxRight(compChoice);
+  //upDateBoxRight(compChoice);
   return compChoice;
 }
 
 function upDateBoxRight(compChoice) {
-
   compChoiceDisplay.textContent = "";
-  compChoiceDisplay.textContent += compChoice + "--" + " " + "score: " + pcScore;
+  compChoiceDisplay.textContent += compChoice + " --" + " " + "Score: " + pcScore;
   scoreBoxRight.appendChild(compChoiceDisplay);
 
 }
@@ -95,14 +131,9 @@ function getRandomInt(max) {
 function checkForTie(pcChoice, personChoice) {
 
   if (pcChoice == personChoice) {
-
-    console.log("");
     return true;
-
   }
 }
-
-
 
 function didPCWin(personChoice, pcChoice) {
   let isTie = checkForTie(personChoice, pcChoice);
@@ -112,9 +143,6 @@ function didPCWin(personChoice, pcChoice) {
   else if (pcChoice == "rock" && personChoice == "scissors") return true;
   else if (pcChoice == "scissors" && personChoice == "paper") return true;
   else return false;
-
-
-
 }
 
 
